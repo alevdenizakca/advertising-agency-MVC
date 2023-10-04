db4cb27c33aebd51cd0427294583d03b3a006a72
alevdenizakca <alevdenizakca@gmail.com>
2023-10-04 15:18:50 +0300

    Add project files.

---
+++ deneme_bigsister_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace deneme_bigsister_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
