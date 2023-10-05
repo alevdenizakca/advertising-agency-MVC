2955b245dbad16f53908eb8c6c6f42220ceaf8a9
alevdenizakca <alevdenizakca@gmail.com>
2023-10-05 16:58:25 +0300

    update

--- deneme_bigsister_1/wwwroot/js/site.js
+++ deneme_bigsister_1/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#carouselExampleIndicators').carousel();
+});
+
+
+$(document).ready(function () {
+    $('#myCarousel').carousel();
+
+    // Next düğmesi için
+    $('.carousel-control-next').click(function () {
+        $('#myCarousel').carousel('next');
+    });
+
+    // Previous düğmesi için
+    $('.carousel-control-prev').click(function () {
+        $('#myCarousel').carousel('prev');
+    });
+});
